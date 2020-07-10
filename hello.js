b54aa5f86bdab115812fded405dd686132efa5d1
Francisco Nvara Ondo <francisco.nvara-ondo@capgemini.com>
2020-07-10 12:45:09 +0200

    Ajouté le objet personne

--- hello.js
+++ hello.js
@@ -1,4 +1,11 @@
-var hello = "Hello!!";
+var hello = "Hello";
 var coucou = "coucou!!!";
+var personne = {
+    id : 01,
+    prenom : "Pierre",
+    nom : "Dubois",
+    age : 45
+};
 alert(hello);
 alert(coucou);
+alert(`${hello} ${personne.prenom} !!`);
